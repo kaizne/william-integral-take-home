7d36dbee751663784bf782f6575111907cc869e9
Kaizne <kaizne@kaizne.com>
2025-07-27 20:39:15 -0700

    fix: fix vercel config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://your-frontend-domain.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Integral')
