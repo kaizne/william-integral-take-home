7b2763bed0ded9339aa64f92d9d46abedae9c50d
Kaizne <kaizne@kaizne.com>
2025-07-27 21:20:47 -0700

    fix: remove cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://your-frontend-domain.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
+  
   const config = new DocumentBuilder()
     .setTitle('Integral')
     .setDescription('Integrations Takehome Challenge')
